9b4928310083be5706f04c6dee4f7b490c803bac
Prathamesh <kosheprathamesh@gmail.com>
2024-10-08 22:03:46 +0530

    related updated event date

--- modules/event.module.js
+++ modules/event.module.js
@@ -1,29 +1,15 @@
 const mongoose = require('mongoose');
 
-const eventSchema = mongoose.Schema({
-    eventTitle:{type:String},
-    eventDate:{type:Date},
-    eventAddress:{type:String},
-    eventOrganizer:{type:String},
-    imageUrl:{type:String},
-    eventPrice:{type: Number},
-    eventDescription:{type:String}
-    
+const event = mongoose.model('event',{
+    eventTitle: { type: String },
+    eventDate: { type: String },
+    eventAddress: { type: String },
+    eventOrganizer: { type: String },
+    imageUrl: { type: String },
+    eventPrice: { type: Number },
+    eventDescription: { type: String }
 });
 
-eventSchema.virtual('formattedEventDate').get(function() {
-    const date = this.eventDate;
-    if (!date) return null;
-    
-    // Format to YYYY-MM-DD
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are 0-based
-    const day = String(date.getDate()).padStart(2, '0');
 
-    return `${year}-${month}-${day}`;
-});
 
-// Set the schema
-const Event = mongoose.model('Event', eventSchema);
-
-module.exports= Event;
+module.exports = event;
