fb78d42807501b674471242faeae6094e708a125
Prathamesh <kosheprathamesh@gmail.com>
2024-11-01 00:31:29 +0530

    changes in cron

--- jobs/cronJobs.js
+++ jobs/cronJobs.js
@@ -3,13 +3,16 @@ const Booking = require('../modules/bookingdetails.module.js');
 const Event = require('../modules/event.module.js');
 
 // Schedule a task to run every hour 
-cron.schedule('*/10 * * * * *', async () => {
+cron.schedule('0 * * * *', async () => {
     try {
 
         const currentDate = new Date();
-        const formattedDate=new Date(currentDate.toISOString().split('T')[0]);
+        const formattedDate=new Date(currentDate);
+        formattedDate.setHours(0, 0, 0, 0);
+        const IST_OFFSET = 5.5 * 60 * 60 * 1000; // IST is UTC+5:30
+        const istMidnight = new Date(formattedDate.getTime() + IST_OFFSET);
         const bookingsToUpdate = await Booking.find({
-            eventDate: { $lt: formattedDate },
+            eventDate: { $lt: istMidnight },
             status: 'Booked' // Only get bookings with status 'Booked'
         });
 
