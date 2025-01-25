f6ff812e2dfe024e0a4b35f56141422623809459
Prathamesh <kosheprathamesh@gmail.com>
2025-01-25 19:55:08 +0530

    auth7

--- routes/bookingdetails.route.js
+++ routes/bookingdetails.route.js
@@ -1,16 +1,17 @@
 let express = require("express");
 let router = express.Router();
 let bookingdetailsController = require('../controllers/bookingdetails.controller');
-let bookingdetailsInterceptor = require('../interceptor/bookingdetails.interceptor')
+let bookingdetailsInterceptor = require('../interceptor/bookingdetails.interceptor');
+let verifyToken = require('../interceptor/auth.interceptor');
 
-router.get('/allBookings', bookingdetailsController.getAllBookings);
-router.get('/userBookings/:userId', bookingdetailsInterceptor.validateUserId, bookingdetailsController.getUserBookings);
-router.get('/eventBookings/:eventId', bookingdetailsInterceptor.validateEventId, bookingdetailsController.getEventBookings);
-router.get('/:bookingId', bookingdetailsInterceptor.validateBookingId, bookingdetailsController.getBookingById);
-router.post('/addBookingDetails', bookingdetailsInterceptor.validateNewBooking, bookingdetailsController.createBooking);
-router.post('/addBookingDetailsByWallet', bookingdetailsInterceptor.validateNewBooking, bookingdetailsController.createBookingWithWallet);
-router.put('/:bookingId', bookingdetailsInterceptor.validateBookingId, bookingdetailsInterceptor.validateUpdateBooking, bookingdetailsController.updateBooking);
-router.delete('/:bookingId', bookingdetailsInterceptor.validateBookingId, bookingdetailsController.deleteBooking);
+router.get('/allBookings', verifyToken, bookingdetailsController.getAllBookings);
+router.get('/userBookings/:userId', verifyToken, bookingdetailsInterceptor.validateUserId, bookingdetailsController.getUserBookings);
+router.get('/eventBookings/:eventId', verifyToken, bookingdetailsInterceptor.validateEventId, bookingdetailsController.getEventBookings);
+router.get('/:bookingId', verifyToken, bookingdetailsInterceptor.validateBookingId, bookingdetailsController.getBookingById);
+router.post('/addBookingDetails', verifyToken, bookingdetailsInterceptor.validateNewBooking, bookingdetailsController.createBooking);
+router.post('/addBookingDetailsByWallet', verifyToken, bookingdetailsInterceptor.validateNewBooking, bookingdetailsController.createBookingWithWallet);
+router.put('/:bookingId', verifyToken, bookingdetailsInterceptor.validateBookingId, bookingdetailsInterceptor.validateUpdateBooking, bookingdetailsController.updateBooking);
+router.delete('/:bookingId', verifyToken, bookingdetailsInterceptor.validateBookingId, bookingdetailsController.deleteBooking);
 
 
 module.exports = router;
