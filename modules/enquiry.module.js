0bff6c227851ec8dc88e2e84be897ace8fcfa191
Prathamesh <kosheprathamesh@gmail.com>
2025-01-25 21:49:20 +0530

    event add id given to user2

--- modules/enquiry.module.js
+++ modules/enquiry.module.js
@@ -2,12 +2,26 @@ const mongoose = require('mongoose');
 
 const EnquirySchema = new mongoose.Schema({
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    userName: { type: String},
     type: { type: String, required: true, enum: ['Organizer Request', 'Event Request', 'Other'] },
     message: { type: String },
     status: { type: String, default: 'Pending', enum: ['Pending', 'Accepted', 'Rejected'] },
     createdAt: { type: Date, default: Date.now },
     fileId: {type: String},
     imageUrl: {type: String}
+});
+
+
+// Automatically fetch and store the userName based on userId
+EnquirySchema.pre('save', async function (next) {
+    if (this.isNew || this.isModified('userId')) {
+        const User = mongoose.model('User'); // Ensure you have the User model
+        const user = await User.findById(this.userId);
+        if (user) {
+            this.userName = user.userName; // Assuming 'userName' is the correct field in your User model
+        }
+    }
+    next();
 });
 
 module.exports = mongoose.model('Enquiry', EnquirySchema);
