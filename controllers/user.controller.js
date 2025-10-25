d793f828dbd8679c6a86cd90f9a36ce25e43ead9
Prathamesh <kosheprathamesh@gmail.com>
2025-10-25 23:51:16 +0530

    sendotp works only localhost

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -69,9 +69,7 @@ async function sendOTP(req, res) {
 
         // Nodemailer Transporter Setup
         const transporter = nodemailer.createTransport({
-            service: 'smtp.gmail.com',
-            port: 465,      
-            secure: true,  
+            service: 'gmail',
             auth: {
                 user: process.env.EMAIL,
                 pass: process.env.EMAIL_PASSWORD
